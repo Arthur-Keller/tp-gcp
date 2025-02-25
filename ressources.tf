b708ad0a83378bf130f2e435b1bf98c1c6370a84
Arthur KELLER <arthur.keller.etu@saule02.iut-infobio.priv.univ-lille1.fr>
2025-02-25 09:57:44 +0100

    yyyy

--- ressources.tf
+++ ressources.tf
@@ -1,24 +1,62 @@
-# Créer un bucket Cloud Storage
-resource "google_storage_bucket" "bucket" {
-  name          = var.bucket_name
-  location      = var.region
-  force_destroy = true
+# Création du bucket Cloud Storage
+resource "google_storage_bucket" "sto_illies" {
+  name          = var.BUCKET
+  location      = var.REGION
+  storage_class = "STANDARD"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 5
+    }
+  }
 }
 
-# Déployer l'application sur Cloud Run
-resource "google_cloud_run_service" "cloud_run" {
-  name     = "flask-app"
-  location = var.region
+
+# Création du Service Account
+resource "google_service_account" "cloud_run_sa" {
+  account_id   = "cloud-run-sa"
+  display_name = "Cloud Run Service Account"
+}
+
+# Attribution des droits IAM au Service Account
+resource "google_project_iam_binding" "cloud_run_access" {
+  project = var.ID
+  role    = "roles/storage.objectViewer"
+  members = ["serviceAccount:${google_service_account.cloud_run_sa.email}"]
+}
+
+
+# Génération de la clé d'accès pour le Service Account
+resource "google_service_account_key" "cloud_run_sa_key" {
+  service_account_id = google_service_account.cloud_run_sa.id
+  key_algorithm      = "KEY_ALG_RSA_2048"
+  private_key_type   = "TYPE_GOOGLE_CREDENTIALS_FILE"
+}
+
+# Déploiement de l'application sur Cloud Run
+resource "google_cloud_run_service" "app_service" {
+  name     = "cloud-run-app"
+  location = var.REGION
 
   template {
     spec {
       containers {
-        image = var.docker_image
+        image = "gcr.io/${var.ID}/flask-tp-ci-cd"
         env {
-          name  = "BUCKET_NAME"
-          value = google_storage_bucket.bucket.name
-        }
+          name  = "BUCKET"
+          value = var.BUCKET
+        
+	}
+
       }
+    service_account_name = google_service_account.cloud_run_sa.email
     }
   }
 
@@ -26,19 +64,23 @@ resource "google_cloud_run_service" "cloud_run" {
     percent         = 100
     latest_revision = true
   }
+  
 }
 
-# Donner l'accès au bucket au service Cloud Run
-resource "google_storage_bucket_iam_member" "cloud_run_access" {
-  bucket = google_storage_bucket.bucket.name
-  role   = "roles/storage.objectAdmin"
-  member = "serviceAccount:${google_cloud_run_service.cloud_run.template[0].spec[0].service_account_name}"	
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
 }
 
-# Rendre le service Cloud Run public
-resource "google_cloud_run_service_iam_member" "all_users_invoker" {
-  location = google_cloud_run_service.cloud_run.location
-  service  = google_cloud_run_service.cloud_run.name
-  role     = "roles/run.invoker"
-  member   = "allUsers"
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.app_service.location
+  project     = google_cloud_run_service.app_service.project
+  service     = google_cloud_run_service.app_service.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
 }
